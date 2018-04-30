cc058caa2621507dcf60416a1cc0bfe4db719b74
Jose Santiago <santiagojoed@gmail.com>
2018-04-30 22:00:20 +0800

    Initial video player

--- app/src/main/java/com/greyblocks/videoplayer/MainActivity.java
+++ app/src/main/java/com/greyblocks/videoplayer/MainActivity.java
@@ -1,13 +1,100 @@
 package com.greyblocks.videoplayer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.AssetDataSource;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.util.Util;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SimpleExoPlayerView playerView;
+    private SimpleExoPlayer player;
+    private boolean playWhenReady = false;
+    private long playbackPosition;
+    private int currentWindow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        playerView = findViewById(R.id.video_view);
+    }
+
+    private void initializePlayer() {
+        player = ExoPlayerFactory.newSimpleInstance(
+                new DefaultRenderersFactory(this),
+                new DefaultTrackSelector(), new DefaultLoadControl());
+
+        playerView.setPlayer(player);
+
+
+
+        Uri uri = Uri.parse("assets:///sample.mp4");
+        MediaSource mediaSource = buildMediaSource(uri);
+        player.prepare(mediaSource, true, false);
+
+        player.setPlayWhenReady(playWhenReady);
+        player.seekTo(0, 0);
+    }
+
+    private MediaSource buildMediaSource(Uri uri) {
+        return new ExtractorMediaSource.Factory(new DataSource.Factory() {
+            @Override
+            public DataSource createDataSource() {
+                return new AssetDataSource(MainActivity.this);
+            }
+        }).setExtractorsFactory(new DefaultExtractorsFactory())
+                .createMediaSource(uri);
+//        return new ExtractorMediaSource.Factory(new DataSource.Factory() {
+//            @Override
+//            public DataSource createDataSource() {
+//                return new AssetDataSource(MainActivity.this);
+//            }
+//        },,
+//                , new DefaultExtractorsFactory(), null, null);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (Util.SDK_INT > 23) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if ((Util.SDK_INT <= 23 || player == null)) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        releasePlayer();
+    }
+
+    private void releasePlayer() {
+        if (player != null) {
+            playbackPosition = player.getCurrentPosition();
+            currentWindow = player.getCurrentWindowIndex();
+            playWhenReady = player.getPlayWhenReady();
+            player.release();
+            player = null;
+        }
     }
 }
